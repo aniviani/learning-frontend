fc06b7b32ed65ee9039e5ca74e2c986e8537fd43
Nastya <anastasiia2004tula@gmail.com>
2026-03-16 15:58:34 +0300

    refactor(Planets): refactor structure

--- src/widgets/Planets/Planets.tsx
+++ src/widgets/Planets/Planets.tsx
@@ -3,24 +3,21 @@ import styles from './Planets.module.css';
 import { usePlanetsQuery } from './model/usePlanetsQuery.ts';
 
 export const Planets = () => {
-  const { isLoading, planets, loadPlanets, error } = usePlanetsQuery();
+  const { isLoading, planets, error } = usePlanetsQuery();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>что-то пошло не так...</div>;
+  }
 
   return (
     <div>
       <div>Planets</div>
-      {isLoading && <div>Loading...</div>}
-      {!planets && (
-        <button
-          onClick={loadPlanets}
-          disabled={isLoading}
-        >
-          загрузить Planets
-        </button>
-      )}
-      {error && <div>что-то пошло не так...</div>}
       <div className={styles.container}>
-        {planets &&
-          planets.map((planet) => (
+        {planets.map((planet) => (
             <Planet
               key={planet.name}
               name={planet.name}
