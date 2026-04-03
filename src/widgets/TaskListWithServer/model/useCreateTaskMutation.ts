350ce6fceeea8836ce83e253c4d7e3c7ff6ebe74
Nastya <anastasiia2004tula@gmail.com>
2026-04-03 11:12:46 +0300

    style(TaskListWithServer): исправлены отступы

--- src/widgets/TaskListWithServer/model/useCreateTaskMutation.ts
+++ src/widgets/TaskListWithServer/model/useCreateTaskMutation.ts
@@ -1,30 +1,34 @@
-import type {Dispatch, SetStateAction} from "react";
-import {tasksApi} from "../api/tasksApi.ts";
-import type {ITaskWithServer} from "../ITaskWithServer.ts";
+import type { Dispatch, SetStateAction } from 'react';
+import { tasksApi } from '../api/tasksApi.ts';
+import type { ITaskWithServer } from '../ITaskWithServer.ts';
 
 interface IParams {
-    setTasks: Dispatch<SetStateAction<ITaskWithServer[] | null>>
+  setTasks: Dispatch<SetStateAction<ITaskWithServer[] | null>>;
 }
 
 interface IResult {
-    createTask: (params: {title: string, description: string}) => Promise<void>;
+  createTask: (params: { title: string; description: string }) => Promise<void>;
 }
 
-export const useCreateTaskMutation = ({setTasks}: IParams): IResult => {
-    const createTask: IResult['createTask'] = async ({title, description}) => {
-       try{
-           const createdTask = await tasksApi.create({id: crypto.randomUUID(), title, completed: false, description})
+export const useCreateTaskMutation = ({ setTasks }: IParams): IResult => {
+  const createTask: IResult['createTask'] = async ({ title, description }) => {
+    try {
+      const createdTask = await tasksApi.create({
+        id: crypto.randomUUID(),
+        title,
+        completed: false,
+        description,
+      });
 
-           setTasks((previousTasks) => {
-               if (previousTasks === null) {
-                   throw new Error('нельзя выполнить задачу')
-               }
-               return [...previousTasks, createdTask];
-           })
-       } catch (error) {
-           console.error(error);
-       }
-
+      setTasks((previousTasks) => {
+        if (previousTasks === null) {
+          throw new Error('нельзя выполнить задачу');
+        }
+        return [...previousTasks, createdTask];
+      });
+    } catch (error) {
+      console.error(error);
     }
-    return {createTask}
-}
+  };
+  return { createTask };
+};
